488ea404b07984c27f9bbe6fa34befc1f86d4a0d
Chris Carty <chriscartydev@gmail.com>
2020-05-18 20:36:33 -0400

    updated go app for rancher master class

--- main.go
+++ main.go
@@ -2,43 +2,21 @@ package main
 
 import (
 	"fmt"
-	"html/template"
 	"net/http"
-
-	"github.com/gorilla/mux"
-	"github.com/prometheus/client_golang/prometheus/promhttp"
 )
 
 func home(w http.ResponseWriter, r *http.Request) {
 
-	t := template.Must(template.ParseFiles("templates/index.html"))
+	fmt.Fprintf(w, "Hello Rancher Master Class")
 
-	t.Execute(w, nil)
-}
-
-// Health Check Endpoint for K8s
-func healthCheck(w http.ResponseWriter, r *http.Request) {
-
-	w.WriteHeader(http.StatusOK)
-	fmt.Fprintf(w, "healthy")
-}
-
-func metrics(w http.ResponseWriter, r *http.Request) {
-
-	w.WriteHeader(http.StatusOK)
-	fmt.Fprintf(w, "healthy")
 }
 
 func main() {
 
-	r := mux.NewRouter()
-
-	r.HandleFunc("/", home)
-	r.HandleFunc("/healthz", healthCheck)
-	r.Handle("/metrics", promhttp.Handler())
+	http.HandleFunc("/", home)
 
 	port := ":8080"
 
 	fmt.Println("Listening on port", port)
-	http.ListenAndServe(port, r)
+	http.ListenAndServe(port, nil)
 }
